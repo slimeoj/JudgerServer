baf63300c3447895b3cd7a3f21f1d65cdc9314c0
rainboy <rainboylvx@qq.com>
2019-04-28 10:52:05 +0800

    加入 测试sock.io的连接

---
+++ test/test_socket.io-connect.js
@@ -0,0 +1,16 @@
+var client = require("socket.io-client")
+
+const socket  = client("http://127.0.0.1:3000")
+
+socket.on('connect',()=>{
+    console.log('connected!')
+})
+
+socket.on('connect_error',(e)=>{
+    console.log('connecte error!')
+    console.log(e)
+})
+
+socket.on('connect_timeout',()=>{
+    console.log('connecte timeout!')
+})
